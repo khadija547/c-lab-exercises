19de435e2517f9431506af0ab7b0291076a6b897
khadija <khadijacloud32gmail.com>
2026-09-05 22:23:49 +0500

    lab3:name age city program

---
+++ lab_03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    char name[50];
+    int age;
+    char city[50];
+
+    printf("enter your name");
+    scanf(" %[^\n]",name);
+
+    printf("enter your age");
+    scanf(" %d", &age);
+
+    printf("enter your city");
+    scanf(" %[^\n]",city);
+
+    printf("Name: %s\n",name);
+    printf("age: %d\n",age);
+    printf("City: %s\n",city);
+
+    return 0;
+}
