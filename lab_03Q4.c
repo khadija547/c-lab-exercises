7daa7ed5c199078e02f7213186558a14e78a1b94
khadija <khadijacloud32gmail.com>
2026-09-06 00:04:07 +0500

    Question 4: temperature program

---
+++ lab_03Q4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    float temprature;
+    
+    printf("enter temprature");
+    scanf(" %f", &temprature);
+
+    printf("temprature: %.2f\n", temprature);
+
+    return 0;
+
+}
