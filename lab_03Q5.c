b2c186b749544eafcb77a55d919ecab37b2e0d1b
khadija <khadijacloud32gmail.com>
2026-09-06 00:16:34 +0500

    Question 5: student table with tabs

---
+++ lab_03Q5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("name\tage\tgrade\n");
+    printf("ali\t20\tA\n");
+    printf("Sara\t21\tB\n");
+    printf("Ahmed\t19\tA\n");
+
+    return 0;
+
+}
