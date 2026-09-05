6e8684d148267423348e7f80e566b3adb56bb971
khadija <khadijacloud32gmail.com>
2026-09-06 00:33:44 +0500

    Question 6: escape sequences program

---
+++ lab_03Q6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("welcome to C progamming\n");
+
+    printf("name:\tali\n");
+    printf("age:\t20\n");
+    printf("grade:\tA\n");
+
+    printf("File Path: C:\\Users\\Student\n");
+
+    printf("He said: \"Hello\"\n");
+
+    return 0;
+}
