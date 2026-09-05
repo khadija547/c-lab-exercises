87b849eac81ac2f9a1c55cd854caf4552874e876
khadija <khadijacloud32gmail.com>
2026-09-06 00:42:48 +0500

    Question 7: single character program

---
+++ lab_03Q7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    char character;
+
+    printf("enter a character");
+    scanf(" %c", &character);
+
+    printf("You entered: %c\n", character);
+
+    return 0;
+}
